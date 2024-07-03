5e94bf0848356deb486bb9319919e67473684147
ye0nj.kim <fulmeach787@gmail.com>
2024-07-03 14:57:12 +0900

    page 42

--- Project11/baseball.cpp
+++ Project11/baseball.cpp
@@ -3,22 +3,30 @@ using namespace std;
 
 class Baseball {
 public:
-	void guess(const string& guessNumber) {
+	bool isDuplicateNumber(const std::string& guessNumber)
+	{
+		return guessNumber[0] == guessNumber[1]
+			|| guessNumber[0] == guessNumber[2]
+			|| guessNumber[1] == guessNumber[2];
+	}
+	void assertIllegalArgument(const std::string& guessNumber)
+	{
 		if (guessNumber.length() != 3) {
 			throw length_error("Must be 3 letters");
 		}
 
 		for (char ch : guessNumber) {
-			if (ch < '0' || ch > '9') {
-				throw invalid_argument("Must be number");
-			}
+			if (ch < '0' || ch > '9') continue;
+			throw invalid_argument("Must be number");
 		}
 
-		if (guessNumber[0] == guessNumber[1]
-			|| guessNumber[0] == guessNumber[2]
-			|| guessNumber[1] == guessNumber[2]) {
+		if (isDuplicateNumber(guessNumber)) {
 			throw invalid_argument("Must not have the same number");
 		}
-	
 	}
-};
+
+
+	void guess(const string& guessNumber) {
+		assertIllegalArgument(guessNumber);
+	}
+	};
